43fdbe3a22a87f098d83b5aa21afb2b84bc50f21
katsenkatorz <lefloch.jf@gmail.com>
2016-10-27 00:55:04 +0200

    Création des bundle
    Ajout du bundle fos user bundle + config :+1:

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -15,7 +15,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new AdminBundle\AdminBundle(),
+            new UserBundle\UserBundle(),
+            new MainBundle\MainBundle(),
+		new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ symfony/src/UserBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+
+	public function __construct()
+	{
+		parent::__construct();
+		// your own logic
+	}
+}
+
